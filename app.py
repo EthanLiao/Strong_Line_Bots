7d9547ac8ca533e749c6e2e38e2033ec59cc1c23
EthanLiao <simon7766112233@gmail.com>
2024-10-03 16:42:01 +0800

    Update app.py

--- app.py
+++ app.py
@@ -87,11 +87,8 @@ def handle_message(event):
         # Get the values from column 'B'
         column_values = get_specific_cell_value(1, 'B')
 
-        # Create a reply message with the retrieved value
-        if "Error" in column_values:
-            reply_message = TextSendMessage(text=f"An error occurred: {column_values}")
-        else:
-            reply_message = TextSendMessage(text=f"Value in cell B1: {column_values}")
+
+        reply_message = TextSendMessage(text=f"Value in cell B1: {column_values}")
 
         line_bot_api.reply_message(event.reply_token, reply_message)
     else:
